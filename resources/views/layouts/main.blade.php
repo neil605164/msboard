aefb3340bfccbe50630abb8004eb74e55effd4fe
Neil Hsieh <neil605164work@gmail.com>
2016-10-15 17:01:35 +0800

    第三次提交

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -80,7 +80,6 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
         <div class="w3-col m3">
             <!-- Profile -->
             <div class="w3-card-2 w3-round w3-white " style="min-height:350px;">
-                I'm in main
                 @yield('information')
             </div>
             <br>
@@ -89,35 +88,17 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
             <div class="w3-card-2 w3-round">
                 <div class="w3-accordion w3-white">
                     <button onclick="myFunction('Demo1')" class="w3-btn-block w3-theme-l1 w3-left-align"><i class="fa fa-circle-o-notch fa-fw w3-margin-right"></i> My Groups</button>
-                    <div id="Demo1" class="w3-accordion-content w3-container">
-                        <p>Some text..</p>
+                    <div id="Demo1" class="w3-accordion-content w3-container" style="min-height:150px;">
+                        @yield('information1')
                     </div>
                     <button onclick="myFunction('Demo2')" class="w3-btn-block w3-theme-l1 w3-left-align"><i class="fa fa-calendar-check-o fa-fw w3-margin-right"></i> My Events</button>
-                    <div id="Demo2" class="w3-accordion-content w3-container">
-                        <p>Some other text..</p>
+                    <div id="Demo2" class="w3-accordion-content w3-container" style="min-height:150px;">
+                        @yield('information2')
                     </div>
                     <button onclick="myFunction('Demo3')" class="w3-btn-block w3-theme-l1 w3-left-align"><i class="fa fa-users fa-fw w3-margin-right"></i> My Photos</button>
-                    <div id="Demo3" class="w3-accordion-content w3-container">
+                    <div id="Demo3" class="w3-accordion-content w3-container" style="min-height:150px;">
                         <div class="w3-row-padding">
-                            <br>
-                            <div class="w3-half">
-                                <img src="img_lights.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
-                            <div class="w3-half">
-                                <img src="img_nature.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
-                            <div class="w3-half">
-                                <img src="img_mountains.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
-                            <div class="w3-half">
-                                <img src="img_forest.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
-                            <div class="w3-half">
-                                <img src="img_nature.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
-                            <div class="w3-half">
-                                <img src="img_fjords.jpg" style="width:100%" class="w3-margin-bottom">
-                            </div>
+                            @yield('information3')
                         </div>
                     </div>
                 </div>
@@ -162,32 +143,14 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
         
             <div class="w3-row-padding">
                 <div class="w3-col m12">
-                    <div class="w3-card-2 w3-round w3-white">
-                        <div class="w3-container w3-padding">
-                            <h6 class="w3-opacity">Social Media template by w3.css</h6>
-                            <p contenteditable="true" class="w3-border w3-padding">Status: Feeling Blue</p>
-                            <button type="button" class="w3-btn w3-theme"><i class="fa fa-pencil"></i>  Post</button>
-                        </div>
+                    <div class="w3-card-2 w3-round w3-white" style="min-height:150px;">
+                        @yield('information4')
                     </div>
                 </div>
             </div>
           
-            <div class="w3-container w3-card-2 w3-white w3-round w3-margin"><br>
-                <img src="img_avatar2.png" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                <span class="w3-right w3-opacity">1 min</span>
-                <h4>John Doe</h4><br>
-                <hr class="w3-clear">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <div class="w3-row-padding" style="margin:0 -16px">
-                        <div class="w3-half">
-                            <img src="img_lights.jpg" style="width:100%" alt="Northern Lights" class="w3-margin-bottom">
-                        </div>
-                        <div class="w3-half">
-                            <img src="img_nature.jpg" style="width:100%" alt="Nature" class="w3-margin-bottom">
-                        </div>
-                    </div>
-                <button type="button" class="w3-btn w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button>
-                <button type="button" class="w3-btn w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button>
+            <div class="w3-container w3-card-2 w3-white w3-round w3-margin" style="min-height:250px;"><br>
+                @yield('information5')
             </div>
           
             <div class="w3-container w3-card-2 w3-white w3-round w3-margin"><br>
